d47ce884a96ee1668dbb25e6a3b221f6f1fcfee6
kylerjohnson26 <kylerljohnson@icloud.com>
2019-12-22 13:27:26 -0500

    WIP: implementing flow to add new list item

--- src/app/shopping-list/add-list-item/add-list-item.component.ts
+++ src/app/shopping-list/add-list-item/add-list-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ListService } from 'src/app/services/list.service';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-list-item',
@@ -8,11 +9,21 @@ import { ListService } from 'src/app/services/list.service';
 })
 export class AddListItemComponent implements OnInit {
 
+  addNewItemForm: FormGroup;
+
   constructor(
+    public formBuilder: FormBuilder,
     private listService: ListService
   ) { }
 
   ngOnInit() {
+    this.addNewItemForm = this.formBuilder.group({
+      name: new FormControl(name)
+    })
+  }
+
+  addNewListItem() {
+    console.log(this.addNewItemForm.value.name)
   }
 
 }
